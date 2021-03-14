5711e77a1b391e09776e1861a1d75897f10f3a43
Tianna <75389500+UselessToaster@users.noreply.github.com>
2021-03-14 04:38:53 -0400

    Add files via upload
    these are the basic components of the google extension, if you go to developer mode in the google extensions page, you can "load unpacked" and select the folder and then run the code by opening another page and searching literally anything because it works on all urls (just sends a message i made up)

---
+++ Samura/content.js
@@ -0,0 +1,13 @@
+
+sentiment = "bad" //variable where the output of the api will be stored (i guess?)
+if (sentiment == "bad") { 
+    message = "Chad sent you a message...aGAIN"; //we can store the messages here, we should have multiple 
+//}else if {
+    //content
+//}else if{
+    //content
+}else {
+    message = "What a lovely message from Stephanie! You should read it when you get a chance ;)";
+};
+
+alert(message) //sends a VERY basic notification with the text (the one that appears at the top of the screen in google sometimes)
