1babf8fcf5d76d0de74a0c4cdf1332c1a66a72d8
Tianna <75389500+UselessToaster@users.noreply.github.com>
2021-03-14 07:35:26 -0400

    Update content.js

--- Samura/content.js
+++ Samura/content.js
@@ -1,13 +1,15 @@
+//will be used to enable push notifications, not functional at the moment
 
-sentiment = "bad" //variable where the output of the api will be stored (i guess?)
-if (sentiment == "bad") { 
-    message = "Chad sent you a message...aGAIN"; //we can store the messages here, we should have multiple 
+
+//sentiment = "bad" //variable where the output of the api will be stored (i guess?)
+//if (sentiment == "bad") { 
+    //message = "Chad sent you a message...aGAIN"; //we can store the messages here, we should have multiple 
 //}else if {
     //content
 //}else if{
     //content
-}else {
-    message = "What a lovely message from Stephanie! You should read it when you get a chance ;)";
-};
+//}else {
+    //message = "What a lovely message from Stephanie! You should read it when you get a chance ;)";
+//};
 
-alert(message) //sends a VERY basic notification with the text (the one that appears at the top of the screen in google sometimes)
+//alert(message) //sends a VERY basic notification with the text (the one that appears at the top of the screen in google sometimes)
